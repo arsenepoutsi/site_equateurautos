f836c057a419a36fd2cf1156f8809d11054b588d
badlee <badinga.ulrich@gmail.com>
2020-09-23 22:29:45 +0000

    add popup modal

--- include/script.php
+++ include/script.php
@@ -58,3 +58,67 @@
 
 <script src="assets/js/aos.js"></script>
 <script src="assets/js/noty.min.js"></script>
+
+
+<!-- Popup modal -->
+<div class="modal fade" id="pubModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document" style="
+    padding: 0;
+    margin: 0 auto;
+    max-width: 75%;
+    max-height: 85%;
+    height: 84%;
+">
+    <div class="modal-content" style="
+    height: 50%;
+    background: url(https://picsum.photos/700/200?i=10&amp;grayscale);
+    background-position: center;
+    background-size: cover;
+">
+      
+      <div class="modal-body" style="
+    /* width: 50%; */
+    /* height: 50%; */
+    /* background: url(https://picsum.photos/700/200?i=10&amp;grayscale); */
+    /* background-position: center; */
+    /* background-size: cover; */
+">
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="
+    background: rgba(255, 255, 255, 0.6);
+    padding: 0px;
+    border-radius: 15px;
+    height: 30px;
+    width: 30px;
+">
+          <span aria-hidden="true">×</span>
+        </button>
+        
+      </div>
+      
+      <div class="modal-footer" style="
+    background-color: #00000066;
+    border: none;
+    height: auto;
+"><div style="
+    text-align: center;
+    color: white;
+    "><h3>Title purus sit amet fermentum</h3>
+    <p>
+Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+    </p>
+        
+        <button type="button" class="btn btn-primary">Action</button>
+      </div>
+</div>
+    </div>
+  </div>
+</div>
+<script>
+	$( window ).load(function() {
+		var q = sessionStorage.getItem("info");
+		if(q == null ){
+			sessionStorage.setItem("info",1);
+			$('#pubModalCenter').modal('show');
+		}
+	});
+</script>
